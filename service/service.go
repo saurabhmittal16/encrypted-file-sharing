8fbd2f9056ecdd3665313f5a9c580743a4ad905f
Saurabh Mittal <saurabhmittal1606@gmail.com>
2020-11-08 20:01:21 +0530

    Add decrypt route

--- service/service.go
+++ service/service.go
@@ -3,18 +3,29 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"encoding/json"
 
 	"github.com/saurabhmittal16/filecryptor"
 )
 
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func RandStringBytes(n int) string {
+    b := make([]byte, n)
+    for i := range b {
+        b[i] = letterBytes[rand.Intn(len(letterBytes))]
+    }
+    return string(b)
+}
+
 type request struct {
 	Path string `json:"path"`
 	Password string `json:"password"`
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
+func handleEncryption(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 
 	var body request
@@ -41,10 +52,41 @@ func root(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+
+func handleDecryption(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+
+	var body request
+	err := decoder.Decode(&body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	encrypted_path := body.Path + ".enc"
+	decrypted_path := RandStringBytes(10) + body.Path[len(body.Path) - 4:]
+
+	err = filecryptor.Decrypt(body.Password, encrypted_path, decrypted_path)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte("Decryption failed"))
+		log.Println("Decryption failed for path: " + body.Path + ". Error message: " + err.Error())
+	} else {
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write([]byte(decrypted_path))
+		log.Println("Decryption successful for path: " + encrypted_path)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
 func main() {
 	port := 8081
 
-	http.HandleFunc("/", root)
+	http.HandleFunc("/encrypt", handleEncryption)
+	http.HandleFunc("/decrypt", handleDecryption)
 	fmt.Printf("Service running at port: %d\n", port)
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
